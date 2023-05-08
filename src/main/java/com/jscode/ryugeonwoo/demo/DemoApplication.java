17ac9de263a9988f06f095249dfc73c3265a3ce8
Ryu. geonwoo <john2318@naver.com>
2023-05-08 22:43:03 +0900

    initial commit

---
+++ src/main/java/com/jscode/ryugeonwoo/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jscode.ryugeonwoo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
